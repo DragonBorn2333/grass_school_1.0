a1d32c085a8d9418597e6bfee720c7e0297f284d
DragonBorn2333 <576250191@qq.com>
2016-12-08 00:43:58 +0800

    占位文件

---
+++ src/main/java/cn/gs/product/www/dao/Test.java
@@ -0,0 +1,15 @@
+package cn.gs.product.www.dao; 
+/** 
+* @author  DragonBorn2333  
+* @version 创建时间：2016年12月8日 上午12:43:13 
+* 类说明 
+*/
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
+ 
+
---
+++ src/main/resources/Test.java
@@ -0,0 +1,14 @@
+/** 
+* @author  DragonBorn2333  
+* @version 创建时间：2016年12月8日 上午12:43:34 
+* 类说明 
+*/
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
+ 
+
